8ae52007f1e450f0146cfad267b27d5ed04b5bd4
0zzycodes <horlalekan123@gmail.com>
2020-01-13 07:27:19 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nodemailer = require("nodemailer");
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json());
@@ -9,6 +10,7 @@ app.use(
     extended: false
   })
 );
+app.use(cors());
 app.get('/', (req, res) => res.send('Server is Running'))
 app.post('/sendmail', async (req, res) => {
   const {
